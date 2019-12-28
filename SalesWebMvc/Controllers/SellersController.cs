8b156427859a540b18644603a07f867e35a19f13
AndersonMarquess <andersongabriel96.ag@gmail.com>
2019-12-28 12:57:21 -0300

    Adicionado funcionalidade de cadastrar vendedor

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
 using SalesWebMvc.Services;
 
 namespace SalesWebMvc.Controllers {
@@ -13,6 +14,17 @@ namespace SalesWebMvc.Controllers {
         public IActionResult Index() {
             var sellers = _sellerService.FindAll();
             return View(sellers);
+        }
+
+        public IActionResult Create() {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller) {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
