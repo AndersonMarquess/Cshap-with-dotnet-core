e1721f1b263ca05e6ef58cffa599440dc915e6e1
AndersonMarquess <andersongabriel96.ag@gmail.com>
2019-12-28 12:00:27 -0300

    Criado serviço para popular banco de dados

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -22,7 +22,12 @@ namespace SalesWebMvc {
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
             ConfigureDatabase(services);
+            RegisterDBService(services);
             services.AddControllersWithViews();
+        }
+
+        private static void RegisterDBService(IServiceCollection services) {
+            services.AddScoped<DBService>();
         }
 
         private void ConfigureDatabase(IServiceCollection services) {
@@ -35,9 +40,10 @@ namespace SalesWebMvc {
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, DBService dBService) {
             if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
+                dBService.PopulateWithMockData();
             } else {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
